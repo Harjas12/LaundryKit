bf2cdc8f3c093a8984508b95b19b7e4e9154c55d
Harjas Monga <harjasmonga@gmail.com>
2019-06-21 22:22:46 -0400

    fixed scoping

--- Sources/LaundryKit/LaundryKit.swift
+++ Sources/LaundryKit/LaundryKit.swift
@@ -1,27 +1,27 @@
 import APIKit
 import Combine
 
-enum LaundryEndpoints: APIResource {
+public enum LaundryEndpoints: APIResource {
     
     case locations
     
-    var method: HTTPMethod {
+    public var method: HTTPMethod {
         switch self {
         case .locations: return .GET
         }
     }
     
-    var endpoint: String {
+    public var endpoint: String {
         switch self {
         case .locations: return "/laundry"
         }
     }
     
-    var base: String {
+    public var base: String {
         return "https://purdue-app-server.herokuapp.com/api/v1"
     }
     
-    var parameters: [String : String]? {
+    public var parameters: [String : String]? {
         return nil;
     }
     
@@ -40,7 +40,7 @@ public struct LaundryRoom: Decodable {
 @available(iOS 13.0, *)
 public struct LaundryService {
     
-    static func getLaundryLocations() -> Publishers.Future<[LaundryRoom], APIError> {
+    public static func getLaundryLocations() -> Publishers.Future<[LaundryRoom], APIError> {
         return APIRequest.getResource(from: LaundryEndpoints.locations, with: EmptyBody(), get: [LaundryRoom].self)
     }
     
